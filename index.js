44d4c6baa4cfb88cfc891285a2e8031d2a74be04
Valerie <valeriejeptoo4@gmail.com>
2023-07-06 11:06:44 +0300

    updated js

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ fetch('http://localhost:3000/films')
     films.forEach((film)=>displayFilmList(film))
 })
 
-//creating a variable for menu
+
 const menu = document.getElementById("menu")
 const filmList =document.getElementById("filmlist")
 
-//adding event listener to the menu 
-//when clicked it will display or not if clicked again
+
 menu.addEventListener("click", function(){
     if(filmList.style.display!=="none"){
         filmList.style.display="none"
@@ -37,28 +36,27 @@ link.addEventListener("click", (e) =>{
     e.preventDefault()
 
 
-    //creating variables for the objects in the json file
+    
     const name = document.querySelector("#headers h3")
     name.innerHTML=`${film.title}`
     console.log(name)
 
-    //displaying the run time
+
     const runtime =document.getElementById("runtime")
     console.log(runtime)
     runtime.textContent=`Runtime : ${film.runtime}`
 
-    //displaying the show time
     const showtime = document.getElementById("showtime")
     console.log(showtime)
     showtime.textContent=`Showtime : ${film.showtime}`
 
-    //displaying the number of available seats
+
     const availableTickets =document.getElementById("availabletickets")
     const result = film.capacity - film.tickets_sold
     console.log(result)
     availableTickets.textContent=`Available Tickets : ${result}`
 
-    //displaying the image
+
     const image = document.querySelector("#section img")
     image.src=film.poster
     console.log(image)
@@ -70,7 +68,7 @@ link.addEventListener("click", (e) =>{
     const mybutton = document.getElementById("mybutton")
     mybutton.innerHTML=`
     <button id="button">BUY TICKET</button>
-    `        //styling the button
+    `       
     let count = result
     const button = document.getElementById("button")
     button.style.backgroundColor="green"
@@ -78,14 +76,14 @@ link.addEventListener("click", (e) =>{
     button.style.height="50px"
     button.style.fontSize="20px"
 
-    //adding event listener to the button 
+    
     button.addEventListener("click", function(){
 
-        //subtracting the the number of available seats
+        
         count--;
         availableTickets.textContent=`Available Tickets : ${count}`
 
-        //preventing the available number from reducing bellow 0
+        
         if(count <= 0){
             button.textContent="Sold out"
             availableTickets.textContent="Available Tickets : Sold out"
